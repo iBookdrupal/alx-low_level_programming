fc8f2f0f0760e67500e7a85a3558f6abcc2f3dbb
iBookdrupal <ternguh@gmail.com>
2022-06-30 14:13:31 -0700

    Printing first programe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include <stdio.h> 
-int main(){
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 puts("Programming is like building a multilingual puzzle");
 getchar();
-return 0;
+return (0);
 }
