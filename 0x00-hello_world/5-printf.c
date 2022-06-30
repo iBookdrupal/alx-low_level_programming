27f6946ec3aef36e5a5a281371bc190a06774a19
iBookdrupal <ternguh@gmail.com>
2022-06-30 16:07:05 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("\nwith proper grammar, but the outcome is a piece of art,");
 getchar();
 return (0);
 }
