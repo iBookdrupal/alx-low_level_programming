3271f233b200a1b422edb704ec1b2cb7c033b6f7
iBookdrupal <ternguh@gmail.com>
2022-07-01 07:06:50 -0700

    Printing lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print to standard output
+ * Return: 0 if exited, compiled without error
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
