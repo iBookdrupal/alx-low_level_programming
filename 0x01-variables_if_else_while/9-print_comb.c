db49889e14501089beba3354662e5380c8478f21
iBookdrupal <ternguh@gmail.com>
2022-07-01 08:32:00 -0700

    printing with space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print to standard output
+ * print lower case base 16 numbers '0-9' and alphabet 'a-f'
+ * Return: 0 if exited, compiled without errors
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		if (num != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		putchar(num);
+		num++;
+	}
+
+
+	putchar('\n');
+
+	return (0);
+}
