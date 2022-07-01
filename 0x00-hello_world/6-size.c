2dd475952e4532ccef0a8a0d72cd1100af273c1b
iBookdrupal <ternguh@gmail.com>
2022-06-30 17:55:50 -0700

    Printing sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main entry point
+ * Return: 0 if compiled without error
+ */
+
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+long longType;
+char charType;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long %zu byte(s)\n", sizeof(doubleType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+return (0);
+}
