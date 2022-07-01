11a5e3d19691591578831d13619616245d1a0db8
iBookdrupal <ternguh@gmail.com>
2022-07-01 07:41:41 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print to standard output
+ * print lowercase a-z excluding 'e' and 'q'
+ * Return: 0 if exited, compiled without error
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	printf("%d", num);
+
+	}
+
+
+	return (0);
+}
