d19ef4b092416cfa6d0b841086e8a7c92a4c2ce2
iBookdrupal <ternguh@gmail.com>
2022-06-30 17:59:42 -0700

    Printing datatypes sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ char charType;
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
-printf("Size of a long long %zu byte(s)\n", sizeof(doubleType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
