f56be37c96b168db58ed0e5312b53dd5ac0497e1
iBookdrupal <ternguh@gmail.com>
2022-07-05 16:51:11 -0700

    multiplication table up to 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+* print_times_table -> print time table;
+* @n: argument
+*/
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+if (n > 15 || n < 0)
+{
+	_putchar(' ');
+}
+else if (n > 0 || n < 15)
+{
+
+	for (i =  0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+		if (k < n && j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		else if (k >= n)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+		}
+		_putchar('\n');
+	}
+}
+}
