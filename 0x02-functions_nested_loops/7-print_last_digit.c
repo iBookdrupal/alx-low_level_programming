5ab93ca6c1d3bfd9e5cce0932b41108347518aeb
iBookdrupal <ternguh@gmail.com>
2022-07-05 14:19:38 -0700

    print last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit -> prints the last digit
+* @last: returns the last digit
+* Return: 0 always
+*
+*/
+
+int print_last_digit(int last)
+{
+	return (last % 10);
+}
