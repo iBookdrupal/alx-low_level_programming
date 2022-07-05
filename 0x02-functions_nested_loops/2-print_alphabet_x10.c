d26abcfe4b6f71448525a48c8269d18c5efa1bdc
iBookdrupal <ternguh@gmail.com>
2022-07-05 12:34:45 -0700

    Printing lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
 
 void print_alphabet_x10(void)
 {
-	int i; 
+	int i;
 	int j;
 
 	for (i = 1; i <= 10; i++)
@@ -15,6 +15,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(j);
 		}
-	}
 	_putchar('\n');
+	}
 }
