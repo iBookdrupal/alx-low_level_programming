c2006998d5b016d26e71dcd4eecdcdb0c1999866
iBookdrupal <ternguh@gmail.com>
2022-07-05 09:34:54 -0700

    Priting _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - entry point
+* printing the variable _putchar
+* Return: always returns 0
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Task 0. Printing _putchar
+Task 1. Write a function that prints the alphabet, in lowercase, followed by a new line.
