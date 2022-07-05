9286f8240111ed4e486aee7ffa724727a14942b3
iBookdrupal <ternguh@gmail.com>
2022-07-05 16:08:13 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,12 +5,31 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
+
 	for (i =  0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(i * j);
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+		if (k < 10 && j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		else if (k >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
 		}
 	}
 }
