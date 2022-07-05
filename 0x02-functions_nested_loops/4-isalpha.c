4a3584f48cf38533ef2c2a97cd14c042dc96f77e
iBookdrupal <ternguh@gmail.com>
2022-07-05 13:18:53 -0700

    checking for alphabetics upper and lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha -> checks for alphabetic characters either capital or lowercase
+* @c: argument for checking the condition
+* Return: 1 if true or 0 otherwise
+*
+*/
+
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
