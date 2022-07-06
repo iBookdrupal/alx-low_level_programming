569abfcbf3d7057f00c9ffb72be6e63f58c503f4
iBookdrupal <ternguh@gmail.com>
2022-07-06 13:35:49 -0700

    finding the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,10 +2,40 @@
 #include "main.h"
 
 /**
-* main - prints the largest of 3 integers
-* Return: 0
+* largest_number - checking for largest number
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: 0, largest number
 */
 
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+return (largest);
+}
+
+
+void positive_or_negative(int i);
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0 always
+*/
 int main(void)
 {
 	int a, b, c;
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,34 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
 
-int largest_number(int a, int b, int c)
-{
-	int largest;
-
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-
-return (largest);
-}
-
+int largest_number(int a, int b, int c);
 
 void positive_or_negative(int i);
 #endif /* MAIN_H */
