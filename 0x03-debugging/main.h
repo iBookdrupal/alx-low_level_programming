1e7fe80070cad71580fb21370dae1931026759f2
iBookdrupal <ternguh@gmail.com>
2022-07-06 10:16:20 -0700

    fixing bugs

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,31 @@
 #define MAIN_H
 
 #include <stdio.h>
+/**
+ * largest_number -> returns the largest of 3 numbers
+ * @i: argument passed
+ * Return: largest
+ */
 
 void positive_or_negative(int i);
 
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+return (largest);
+}
 #endif /* MAIN_H */
