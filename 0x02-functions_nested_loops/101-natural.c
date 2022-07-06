b23ecdfd04db654695df689ac01c0606b528e9a3
iBookdrupal <ternguh@gmail.com>
2022-07-05 17:44:26 -0700

    sum of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include "main.h"
-/**
-* print_sum_of_numbers -> printing sum of numbers
-* Return: 0 always
-*/
 
-int print_sum_of_numbers(void)
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	int sum3, sum5;
+	unsigned long int sum3, sum5, sum;
 	int i;
 
-	for (i = 0; i < 1024; i++)
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
 	{
-		if (i % 3 == 0)
+		if ((i % 3) == 0)
 		{
-			sum3 += i;
-		}
-		else if (i % 5 == 0)
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
 		{
-			sum5 += i;
+			sum5 = sum5 + i;
 		}
 	}
-
-	printf("%d\n", sum3, sum5);
-
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
 	return (0);
 }
