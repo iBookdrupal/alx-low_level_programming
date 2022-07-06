73cc708e8d9fe10ba9a4fa827df116d52a82d16d
iBookdrupal <ternguh@gmail.com>
2022-07-05 17:14:53 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,47 @@
 #include "main.h"
-/**
-* print_times_table -> print time table;
-* @n: argument
-*/
 
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
 void print_times_table(int n)
 {
 	int i, j, k;
 
-if (n > 15 || n < 0)
-{
-	_putchar(' ');
-}
-else if (n > 0 || n < 15)
-{
-
-	for (i =  0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = j * i;
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(k + '0');
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-
-		if (k <= n && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
+			_putchar('\n');
 		}
-		else if (k >= n)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-		}
-		}
-		_putchar('\n');
 	}
-}
 }
