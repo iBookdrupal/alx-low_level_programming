53d8f0727b4c06ca0a3ac83d8c0ad6f0bea914f0
iBookdrupal <ternguh@gmail.com>
2022-07-06 00:51:45 -0700

    printing fibonnacci 98 number

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,24 +10,35 @@
 
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next;
 
-	j = 0;
-	k = 1;
+unsigned long int i, j, k, j1, j2, k1, k2;
 
-	for (i = 1; i <= 99; i++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		next = j + k;
-		if (i != 99)
-		{
-			printf("%lu, ", next);
-		}
+		printf("%lu", k);
+		k = k + j;
+		j = k - j;
+	}
 
-		j = k;
-		k = next;
-		}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
 
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
 	printf("\n");
 	return (0);
 }
