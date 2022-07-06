b982fc690886d339910e91595de3e0a8196804ee
iBookdrupal <ternguh@gmail.com>
2022-07-06 13:38:36 -0700

    check largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -30,23 +30,3 @@ return (largest);
 }
 
 
-void positive_or_negative(int i);
-
-/**
-* main - prints the largest of 3 integers
-* Return: 0 always
-*/
-int main(void)
-{
-	int a, b, c;
-	int largest;
-
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-	printf("%d is the largest number\n", largest);
-
-	return (0);
-}
