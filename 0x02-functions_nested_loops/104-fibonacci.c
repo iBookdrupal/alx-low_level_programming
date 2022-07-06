dece0d7ed01cc76534580eab24a3e3aff7339e51
iBookdrupal <ternguh@gmail.com>
2022-07-05 23:37:23 -0700

    printing fibbonacci number 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that finds and prints the first 98 Fibonacci
+ *              numbers, starting with 1 and 2
+ *
+ * Return: Always 0 successful
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 98; i++)
+	{
+		next = j + k;
+		if (i != 98)
+		{
+			printf("%lu, ", next);
+		}
+
+		j = k;
+		k = next;
+		}
+
+	printf("\n");
+	return (0);
+}
