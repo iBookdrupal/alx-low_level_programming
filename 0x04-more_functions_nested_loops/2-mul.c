96940e5bba450729f3b7512d1208e2fdc4ada718
iBookdrupal <ternguh@gmail.com>
2022-07-07 11:48:37 -0700

    multiply twonumbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> multiply two numbers in function
+ * @a: argument a
+ * @b: argument b
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
