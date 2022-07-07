dd5d491224997805f33f3c4e6e273f0e916c64f8
iBookdrupal <ternguh@gmail.com>
2022-07-07 13:16:14 -0700

    Printing numbers from 1-14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
-* more_numbers - printing 1-14 ten times 
+* more_numbers - printing 1-14 ten times
 *
 */
 
 void more_numbers(void)
 {
 	int i, j;
-	for (i = 0; i <= 10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-		for (j =; j<= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+
+			_putchar(j % 10 + '0');
 		}
-	}
 
 	_putchar('\n');
+	}
+
 
 }
