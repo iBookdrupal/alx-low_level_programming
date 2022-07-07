0c28069176738236f73663beb0d55241f3e8daa3
iBookdrupal <ternguh@gmail.com>
2022-07-07 12:08:54 -0700

    print number from 0 to 9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+
 void print_numbers(void);
+
+/**
+* _putchar - write character c to standard output
+* @c: argument, parameter, character to check
+* Return: 0 or 1
+*/
+
+int _putchar(char c);
+
+
+
+#endif
