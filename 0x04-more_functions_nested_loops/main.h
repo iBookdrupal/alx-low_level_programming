74cf8b16de238fe77940cb71494f16b4cd40402d
iBookdrupal <ternguh@gmail.com>
2022-07-07 12:32:37 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,20 +1,44 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/**
+*_isupper -> Check for upper case
+*@c: argument c
+* Return: always 0
+*/
 int _isupper(int c);
+
+/**
+*_isdigit -> check for digit numbers
+*@c: argument c
+* Return: always 0
+*/
 int _isdigit(int c);
+
+/**
+*mul -> multiply two numbers a and b
+* @a: argument 1
+* @b: argument 2
+* Return: always 0
+*/
 int mul(int a, int b);
 
+/**
+* print_numbers -> print numbers from 0 to 9
+*/
 void print_numbers(void);
 
 /**
 * _putchar - write character c to standard output
-* @c: argument, parameter, character to check
+* @c: parameter, character to check
 * Return: 0 or 1
 */
 
 int _putchar(char c);
 
+/**
+* print most numbers
+*/
 
-
+void print_most_numbers(void);
 #endif
