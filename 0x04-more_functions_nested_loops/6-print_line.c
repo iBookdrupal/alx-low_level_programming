1388916a7f993d2459a127caf26048e817412e3e
iBookdrupal <ternguh@gmail.com>
2022-07-07 21:26:30 -0700

    printing line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,21 +6,18 @@
 
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	if (n < 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; ++i)
 		{
 			_putchar('_');
 		}
 	}
 
-	_putchar('\n');
-
-	return (0);
 }
