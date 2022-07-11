546a065e11c0850b5979afaaaa83eef03a03d53b
iBookdrupal <ternguh@gmail.com>
2022-07-11 09:17:08 -0700

    printin fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,21 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf(" Fizz");
-		if (i % 5 == 0)
+		}
+		else if (i % 5 == 0)
+		{
 			printf(" Buzz ");
-		if (i % 15 == 0)
+		}
+		else if (i % 15 == 0)
+		{
 			printf(" FizzBuzz ");
+		}
 		else
+		{
 			printf(" %d", i);
+		}
 	}
 	printf("\n");
 	return (0);
