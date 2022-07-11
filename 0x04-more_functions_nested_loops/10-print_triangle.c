de49697cbe7abb001883279164fa1d25ca825c6d
iBookdrupal <ternguh@gmail.com>
2022-07-11 13:17:47 -0700

    printing triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_triangle - check the code
+* @size: size of the triangle
+* Return: Always 0.
+*/
+
+void print_triangle(int size)
+{
+	int i, space, k = 0;
+
+	for (i = 1; i <= size; ++i; k = 0)
+	{
+		for (space = 1; space <= size - 1; ++space)
+		{
+			_putchar(' ');
+		}
+
+		while (k != 2 * i - 1)
+		{
+			_putchar('# ');
+			++k;
+		}
+
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -59,5 +59,7 @@ void print_line(int n);
 void print_diagonal(int n);
 
 void print_square(int size);
+
+void print_triangle(int size);
 #endif
 
