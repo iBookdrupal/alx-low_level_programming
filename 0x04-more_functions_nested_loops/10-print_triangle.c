9d5ccec2b99c2147c9f502467243e7cec38c2dca
iBookdrupal <ternguh@gmail.com>
2022-07-11 13:27:05 -0700

    printing triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,7 @@ void print_triangle(int size)
 {
 	int i, space, k = 0;
 
-	for (i = 1; i <= size; ++i; k = 0)
+	for (i = 1; i <= size; ++i)
 	{
 		for (space = 1; space <= size - 1; ++space)
 		{
@@ -19,7 +19,8 @@ void print_triangle(int size)
 
 		while (k != 2 * i - 1)
 		{
-			_putchar('# ');
+			_putchar(' ');
+			_putchar('#');
 			++k;
 		}
 
