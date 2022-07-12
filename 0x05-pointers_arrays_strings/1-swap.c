b62bc476b98ad436014d62c6fcf6d3060c11f82a
iBookdrupal <ternguh@gmail.com>
2022-07-12 18:48:36 +0100

    swapping numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/**
+* swap_int - swapping integers
+* @a: parameter 1
+* @b: parameter 2
+*
+*/
+
+void swap_int(int *a, int *b)
+{
+	int i, j;
+
+	i = *a; 
+	j = *b;
+
+	*a = j;
+	*b = i;
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,12 @@
 */
 void reset_to_98(int *n);
 
+
+/**
+* swap_int - swapping integers
+* @a: parameter a passed
+* @b: parameter b passed
+*/
+void swap_int(int *a, int *b);
+
 #endif
