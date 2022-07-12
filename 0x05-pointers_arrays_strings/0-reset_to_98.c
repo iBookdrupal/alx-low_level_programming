0c5617f886c269114a47fa0d89ae3322e8f947c6
iBookdrupal <ternguh@gmail.com>
2022-07-12 18:29:38 +0100

    reset pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+* reset_to_98 - function to reset pointer
+* @n: reset parameter
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+* reset_to_98 - reseting
+* @n: parameter passed
+*/
+void reset_to_98(int *n);
+
+#endif
