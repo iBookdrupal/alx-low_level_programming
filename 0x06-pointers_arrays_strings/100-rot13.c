04a65c263146313c2227a2db433f27cb1f27ea8d
iBookdrupal <ternguh@gmail.com>
2022-07-15 05:53:32 +0100

    Rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,14 +6,23 @@
 *
 */
 
-char *rot13(char *src)
+char *rot13(char *s)
 {
-	for (; *src; src++)
+	rint i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((*src > 'A' && *src < 'N') || (*src > 'a' && *src < 'n')) 
-		*src += 13;
-		else if ((*src > 'M' && *src < 'Z') || (*src > 'm' && *src < 'z')) 
-		*src -= 13;
-    }
-	return (src);
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
