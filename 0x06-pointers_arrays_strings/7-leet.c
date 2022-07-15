62da2736f2ace0ef8aa6b74b12345dce8f96060b
iBookdrupal <ternguh@gmail.com>
2022-07-15 05:15:26 +0100

    leet encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* leet - encone letters with numbers
+* @s: leet parameter
+* Return: nothing
+*
+*/
+
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+		
+
+	}
+	return (s);
+}
