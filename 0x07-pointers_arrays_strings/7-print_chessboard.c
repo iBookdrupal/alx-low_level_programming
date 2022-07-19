5a35003bc88514ea26e14a0f963e058fada13de7
iBookdrupal <ternguh@gmail.com>
2022-07-19 11:04:24 +0100

    printing Chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_chessboard -> a function that prints the chessboard.
+* @a: array pointer
+* Return: 0 always successful
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	char *ptr = (char *)a;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", *ptr);
+			ptr++;
+
+			if (j == 7)
+			{
+				printf("\n");
+			}
+		}
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
-int compare(char *, char *);
+
+void print_chessboard(char (*a)[8]);
 
 #endif
