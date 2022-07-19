fab65b8f1e989d31ef3987956e7a1c716d6d4162
iBookdrupal <ternguh@gmail.com>
2022-07-19 18:35:54 +0100

    sum of diagonal matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_diagsums -> sum of the two diagonals of a square matrix of integers.
+* @a: array pointer
+* @size: array size
+* Return: 0 always successful
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, size1;
+	unsigned int sumD1, sumD2;
+
+	size1 = 0;
+	sumD1 = 0;
+	sumD2 = 0;
+
+	t_size = (size * size) - 1;
+
+
+	for (i = 0; i <= t_size; i = i + (size + 1))
+	{
+		sumD1 = sumD1 + a[i];
+	}
+
+	for (i = (size - 1); i <= t_size; i = i + (size + 1))
+	{
+		sumD2 = sumD2 + a[i];
+	}
+
+	printf("%d", "%d\n", sumD1, sumD2);
+}
