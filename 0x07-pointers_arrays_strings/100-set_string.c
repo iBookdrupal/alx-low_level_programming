04e151ad823ff9488089014d513f23456eb3959f
iBookdrupal <ternguh@gmail.com>
2022-07-19 18:50:30 +0100

    set for double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* set_string -> set string to char.
+* @s: double pointer
+* @to: single pointer
+* Return: 0 always successful
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
