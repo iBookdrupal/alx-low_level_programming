ae1978ff9a3e5edc0a729d24e0992a387aedc574
iBookdrupal <ternguh@gmail.com>
2022-07-20 22:08:20 +0100

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strlen_recursion - string length in number
+* @s: string parameter
+* Return: integer
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+	_putchar('\n');
+
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,8 @@ int _putchar(char c);
 
 void _print_rev_recursion(char *s);
 
+int _strlen_recursion(char *s);
+
 
 
 
