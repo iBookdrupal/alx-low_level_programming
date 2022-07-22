222cd35c76ee7efaecefc58f21e2f716bf653654
iBookdrupal <ternguh@gmail.com>
2022-07-22 03:00:22 +0100

    compare strings with special characters

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-int primeno(int num, int i);
-
+int _strcheck(char *s1, char *s2, char *c);
 /**
 * wildcmp - palidrome
 * @s1: character string
@@ -9,42 +8,62 @@ int primeno(int num, int i);
 * Return: number
 */
 
-
 int wildcmp(char *s1, char *s2)
 {
-	if (n <= 1)
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (_strcheck(s1, (s2 + 1), (s2 + 1)));
+	}
+	else
 	{
 		return (0);
 	}
-		return (primeno(n, n - 1));
 }
-
 
 
 
 /**
-* primeno - prime number function
-* @i: prime numbers iterator
-* @num: prime number
-* Return: prime number
+* _strcheck - palidrome
+* @s1: character string
+* @s2: special character
+* @c: for wild card
+* Return: number
 */
 
-int primeno(int num, int i)
+int _strcheck(char *s1, char *s2, char *c)
 {
-	if (i == 1)
+	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
+	if (*s1 == '\0' && *s2 == '*')
+	{
+		return (_strcheck(s1, s2 + 1, s2 + 1));
+	}
+	if (*s1 == '\0' && *s2 != '\0')
+	{
+		return (0);
+	}
+	if (*s2 == '*')
+	{
+		return (_strcheck(s1, s2 + 1, s2 + 1));
+	}
+	if (*s1 == *s2)
+	{
+		return (_strcheck(s1 + 1, s2 + 1, c));
+	}
 	else
 	{
-		if (num % i == 0 && i > 0)
-		{
-			return (0);
-		}
-		else
-		{
-			return (primeno(num, i - 1));
-		}
+		return (_strcheck(s1 + 1, c, c));
 	}
-}
 
+}
