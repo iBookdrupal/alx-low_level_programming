c2d6c1afde48aa61d745b6ba2ad61ab4b41be87a
iBookdrupal <ternguh@gmail.com>
2022-07-23 02:07:22 +0100

    argument counts

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print program argument count
+* Return: 0 or 1 for success
+* @argc: argument count
+* @argv: argument vector
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
+
