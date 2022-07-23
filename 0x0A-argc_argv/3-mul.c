9e69ed520dfaf8cbbc734cd18cdff891dc303683
iBookdrupal <ternguh@gmail.com>
2022-07-23 02:31:29 +0100

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - multiplication of two numbers
+* Return: 0 or 1 for success
+* @argc: argument count
+* @argv: argument vector
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (1);
+}
+
