5cd0726c65c48471c76cc1bec9c7f8b435e59def
iBookdrupal <ternguh@gmail.com>
2022-07-23 22:04:57 +0100

    add natural nums esc chars

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,9 +13,6 @@ int main(int argc, char *argv[])
 {
 	int i, j, k, sum = 0;
 
-	if (argc == 1)
-		printf("0\n");
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -33,5 +30,7 @@ int main(int argc, char *argv[])
 		sum += atoi(argv[k]);
 	}
 	printf("%d\n", sum);
+
+	return (0);
 }
 
