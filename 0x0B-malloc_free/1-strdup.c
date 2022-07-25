675911306351cf9a1d27e693dc8ef84cd7e6086a
iBookdrupal <ternguh@gmail.com>
2022-07-25 23:30:00 +0100

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+* _strdup - create a newly allocated space
+* @str: new string
+* Return: duplicate of string str
+*/
+char *_strdup(char *str)
+{
+	int i = 1, j = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	str = (char *) malloc(i * sizeof(char) + 1);
+	if (p == NULL)
+		return (NULL);
+
+	while (j < i)
+	{
+		p[j] = str[j];
+		j++;
+	}
+	p[j] = '\0';
+
+	free(p);
+	return (p);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,6 +1,6 @@
 # 0x0B. C - malloc, free
 ## Resources
 * [0x0a - malloc & free ](https://alx-intranet.hbtn.io/rltoken/7q6RmWq86XkUhvmlhrg9bg)
-* []Dynamic memory allocation in C - malloc calloc realloc free (stop at 6:50)()https://alx-intranet.hbtn.io/rltoken/pfGb2oVIYLO_1a8jtFGQYw
+* [Dynamic memory allocation in C - malloc calloc realloc free - stop at 6:50 (https://alx-intranet.hbtn.io/rltoken/pfGb2oVIYLO_1a8jtFGQYw)
 
 
