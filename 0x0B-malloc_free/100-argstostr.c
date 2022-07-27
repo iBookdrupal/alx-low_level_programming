0c5bb073a37a7652d70ccee318a7fc8e9ae85450
iBookdrupal <ternguh@gmail.com>
2022-07-27 20:55:16 +0100

    concatenates string arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* argstostr - argument to string.
+* @ac: argument counter
+* @av: argument variable
+* Return: Always 0.
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, length;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+
+		length++;
+	}
+
+	str = malloc(sizeof(char) * (length + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+
+	return (str);
+}
