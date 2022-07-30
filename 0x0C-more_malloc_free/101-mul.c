5e52a9fe119f44a9bd6feeabb073dc0671602150
iBookdrupal <ternguh@gmail.com>
2022-07-30 03:51:49 +0100

    arbitrary calculator

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,20 +1,19 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * is_digit - check for digit in string character
- * @s: string
- * Return: 0 or 1
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-
 int is_digit(char *s)
 {
 	int i = 0;
 
 	while (s[i])
 	{
-		if (s[i] < '\0' || s[i] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
 	}
@@ -22,11 +21,11 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - checks string length
- * @s: string
- * Return: string length
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
 	int i = 0;
@@ -39,26 +38,25 @@ int _strlen(char *s)
 }
 
 /**
- * errors - main error
+ * errors - handles errors for main
  */
-
 void errors(void)
 {
-	printf("Errors\n");
+	printf("Error\n");
 	exit(98);
 }
 
 /**
  * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 or 1
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, mult, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -69,38 +67,28 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	i = 0;
-	while (i <= len1 + len2)
-	{
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-		i++;
-	}
-	len1 = len1 - 1;
-	while (len1 >= 0)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
-		mult = 0;
-		len2 = _strlen(s2) - 1;
-		while (len2 >= 0)
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
-			mult += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = mult % 10;
-			mult /= 10;
-			len2--;
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (mult > 0)
-			result[len1 + len2 + 1] += mult;
-		len1--;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	i = 0;
-	while (i < len - 1)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
 		if (a)
 			_putchar(result[i] + '0');
-		i++;
 	}
 	if (!a)
 		_putchar('0');
@@ -108,3 +96,4 @@ int main(int argc, char *argv[])
 	free(result);
 	return (0);
 }
+
