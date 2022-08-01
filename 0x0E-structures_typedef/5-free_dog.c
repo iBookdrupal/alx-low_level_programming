72fa3937c5f6359b0698597cfcf436686f1ba0a6
iBookdrupal <ternguh@gmail.com>
2022-08-01 22:34:00 +0100

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*free_dog - free dog memory
+*@d: free pointer dog
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return;
+	free(d->name);
+	free(d->owner = owner);
+	free(d);
+}
+
+
