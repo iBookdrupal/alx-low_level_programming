2973fbf3c374e877571e071ae6c56e4f64161654
iBookdrupal <ternguh@gmail.com>
2022-08-04 03:05:21 +0100

    print array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+* array_iterator - array interator
+* @array: array
+* @size: Array size
+* @action: pointer function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i])
+			i++;
+	}
+}
+
