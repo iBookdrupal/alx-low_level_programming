23c75141f5ae47738191837872338d7e0bda44ec
iBookdrupal <ternguh@gmail.com>
2022-08-05 02:28:53 +0100

    Print upcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stidlib.h>
+
+/**
+* main - upcode
+* @argc: argument count
+* @argv: argument vecdor
+* Return: 0 sucess always
+*
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int count, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (count = 0; count < bytes; count++)
+	{
+		printf("%02hhx", *((char)main + count));
+		if (count < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
+
