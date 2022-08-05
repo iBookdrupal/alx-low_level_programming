6af009f02f52310b07d4eed7a565e73e8559fe1a
iBookdrupal <ternguh@gmail.com>
2022-08-05 03:56:12 +0100

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+/**
+* print_numbers - printing numbers
+*@separator: separator sign
+*@n: number to print
+*
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numbers;
+
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numbers, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(numbers);
+}
