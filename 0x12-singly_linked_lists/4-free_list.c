d2e787fca0916ec994b11d3b2210a6944f393fad
iBookdrupal <ternguh@gmail.com>
2022-08-12 04:00:13 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+* free_list - free lists
+* @head: pointer head
+* Return: void
+*/
+
+void free_list(list_t *head);
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->next);
+		free(head);
+		head = temp;
+	}
+}
