c6099f50f8f501c0fecac58341fd8f57ab745cdd
iBookdrupal <ternguh@gmail.com>
2022-08-12 03:27:46 +0100

    add element at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* add_node - add element at the front
+* @head: pointer head
+* @str: string of elements
+* Return: nodes
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int i;
+
+	while (str[i])
+		i++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = i;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
