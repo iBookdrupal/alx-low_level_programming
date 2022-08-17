93ee8d1ef7949463c12b51a82c065a2897a486d4
iBookdrupal <ternguh@gmail.com>
2022-08-17 05:51:07 +0100

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+* delete_nodeint_at_index - delete node at any index
+* @index: index to insert
+* @head: head of pointer
+*
+* Return: return node list where inserted
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int node;
+	listint_t *copy, *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+
+	if (copy == NULL)
+	return (-1);
+
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
+
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (copy->next)
+		return (-1);
+
+		copy = copy->next;
+	}
+
+	temp = copy->next;
+	copy->next = temp->next;
+	free(temp);
+	return (1);
+}
