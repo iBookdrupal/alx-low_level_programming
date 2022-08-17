abdb2ab378c586087ee264e936645b119b02fc70
iBookdrupal <ternguh@gmail.com>
2022-08-17 03:45:58 +0100

    free list nodes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint - free elements
+* @head: head of pointer
+* Return: return a freed list of elements
+*/
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+
+	while (*head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+
+}
+
