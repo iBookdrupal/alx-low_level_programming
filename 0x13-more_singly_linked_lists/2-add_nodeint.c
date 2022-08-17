cc33f527180c1d06daf83a96205ce89e5bf7f8c5
iBookdrupal <ternguh@gmail.com>
2022-08-17 03:14:55 +0100

    add at the beginning of a node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,16 +12,17 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	new_node = (listint_t *) malloc(sizeof(listint_t);
+	new_node = (listint_t *) malloc(sizeof(listint_t));
 
 
-	while (new_node != NULL)
+	if (new_node != NULL)
 	{
-		new_node->n;
+		new_node->n = n;
 		new_node->next = *head;
 	}
 	else
-		return (NULL)
+		return (NULL);
+
 	if (*head != NULL)
 		new_node->next = *head;
 
