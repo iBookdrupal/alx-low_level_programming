4b2d1acc390c76063cc9200cecb27e0c4c0a17dc
iBookdrupal <ternguh@gmail.com>
2022-08-17 02:44:21 +0100

    print length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,16 +3,14 @@
 /**
 * listint_len - print length of elements
 * @h: pointer
-* Return: return lenth of elements
+* Return: return length of elements
 */
 
 size_t listint_len(const listint_t *h);
 {
 	size_t count = 0;
-	const listint_t *current;
+	const listint_t *current = h;
 
-	current = h;
-	count = 0;
 
 	while (current != NULL)
 	{
