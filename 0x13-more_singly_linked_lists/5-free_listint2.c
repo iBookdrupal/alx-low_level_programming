f936877436f580c32570c968cd1d4293fbd047da
iBookdrupal <ternguh@gmail.com>
2022-08-17 03:56:14 +0100

    free list with pointer of pointers

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free elements
+* @head: head of pointer
+* Return: return a freed list of elements
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+	return;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+	head = NULL;
+}
+
