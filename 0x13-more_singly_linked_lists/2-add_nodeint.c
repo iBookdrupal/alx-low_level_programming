375466410656194027235f383ff1029611a8c438
iBookdrupal <ternguh@gmail.com>
2022-08-17 03:09:24 +0100

    add node at the beginnign

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+* add_nodeint - add node at the beginning
+* @head: head of list
+* @n: number count
+*
+* Return: return length of elements
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = (listint_t *) malloc(sizeof(listint_t);
+
+
+	while (new_node != NULL)
+	{
+		new_node->n;
+		new_node->next = *head;
+	}
+	else
+		return (NULL)
+	if (*head != NULL)
+		new_node->next = *head;
+
+	*head = new_node;
+	return (new_node);
+}
+
