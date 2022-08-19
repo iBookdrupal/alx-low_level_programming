666a7111a6955de95449ff28df4940deb2ed68d9
iBookdrupal <ternguh@gmail.com>
2022-08-19 05:12:16 +0100

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 
 
